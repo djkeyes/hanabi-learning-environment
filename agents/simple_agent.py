30e7209f91124e523d8b03aa0421ee48f9ea58a4
Daniel Keyes <danieljkeyes@gmail.com>
2019-11-20 16:36:01 -0600

    Revert "make SimpleAgent extremely cautious if only 1 life is left"
    This reverts commit c8e223b289a6adf61b8e8a50da07b2241817c337.

--- agents/simple_agent.py
+++ agents/simple_agent.py
@@ -36,12 +36,8 @@ class SimpleAgent(Agent):
     # Check if there are any pending hints and play the card corresponding to
     # the hint.
     for card_index, hint in enumerate(observation['card_knowledge'][0]):
-      if hint['color'] is not None and hint['rank'] is not None:
+      if hint['color'] is not None or hint['rank'] is not None:
         return {'action_type': 'PLAY', 'card_index': card_index}
-      # But don't be too dumb if we're about to loose
-      if observation['life_tokens'] > 1:
-        if hint['color'] is not None or hint['rank'] is not None:
-          return {'action_type': 'PLAY', 'card_index': card_index}
 
     # Check if it's possible to hint a card to your colleagues.
     fireworks = observation['fireworks']
@@ -52,26 +48,23 @@ class SimpleAgent(Agent):
         player_hints = observation['card_knowledge'][player_offset]
         # Check if the card in the hand of the opponent is playable.
         for card, hint in zip(player_hand, player_hints):
-          if SimpleAgent.playable_card(card, fireworks):
-            if hint['color'] is None:
-              return {
-                  'action_type': 'REVEAL_COLOR',
-                  'color': card['color'],
-                  'target_offset': player_offset
-              }
+          if SimpleAgent.playable_card(card,
+                                       fireworks) and hint['color'] is None:
+            return {
+                'action_type': 'REVEAL_COLOR',
+                'color': card['color'],
+                'target_offset': player_offset
+            }
 
-    # If no card is hintable then discard or hint arbitrarily.
+    # If no card is hintable then discard or play.
     if observation['information_tokens'] < self.max_information_tokens:
       return {'action_type': 'DISCARD', 'card_index': 0}
     else:
-      return {
-          'action_type': 'REVEAL_COLOR',
-          'color': observation['observed_hands'][1][0]['color'],
-          'target_offset': 1
-      }
+      return {'action_type': 'PLAY', 'card_index': 0}
 
   def reset(self, config):
     # Refresh config, in case it changed
     self.config = config
     # Extract max info tokens or set default to 8.
     self.max_information_tokens = config.get('information_tokens', 8)
+
